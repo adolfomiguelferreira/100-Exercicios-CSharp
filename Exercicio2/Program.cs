ef424a7cd14817d587cfeb04b18f2955628b8926
Adolfo Miguel Ferreira <adolfomiguelferreira16@gmail.com>
2025-10-30 01:03:56 +0200

    Exercício 2
    2. Crie um programa fonte de uma aplicação em C# que receba o
    nome e sobre nome de uma pessoa e imprime na tela o nome
    completo.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobreNome;
+
+            Console.Write("Escreva o seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Escreva o seu Sobrenome: ");
+            sobreNome = Console.ReadLine();
+
+            Console.WriteLine(string.Format("O seu Nome é {0} Sobrenome {1}.", nome, sobreNome));
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Exercicio2/README.md
@@ -0,0 +1,3 @@
+2. Crie um programa fonte de uma aplicação em C# que receba o
+nome e sobre nome de uma pessoa e imprime na tela o nome
+completo. 
