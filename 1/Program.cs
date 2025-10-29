601b46af7076fc64652f31fd989ece2c39c87e74
Adolfo Miguel Ferreira <adolfomiguelferreira16@gmail.com>
2025-10-30 00:51:23 +0200

    Exercício 1
    1. Escreva um programa C# que imprima na tela “Olá Teu nome”.

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Adolfo Ferreira");
+            Console.ReadKey();
+        }
+    }
+}
+
